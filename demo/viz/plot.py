8bd1f1132d84b06cbebb8450ac8d7c20cc8eaa31
iver56 <iver56@hotmail.com>
2021-10-22 11:24:46 +0200

    wip

---
+++ demo/viz/plot.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+
+
+def plot_waveforms(wf1, wf2=None, wf3=None, title="Untitled"):
+    """Plot one, two or three short 1D waveforms. Useful for debugging."""
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_title(title)
+    plt.plot(wf1, label="wf1", alpha=0.66)
+    if wf2 is not None:
+        plt.plot(wf2, label="wf2", alpha=0.66)
+    if wf3 is not None:
+        plt.plot(wf3, label="wf3", alpha=0.66)
+    plt.legend()
+    plt.show()
+    plt.close(fig)
+
+
+def plot_matrix(matrix, output_image_path=None, vmin=None, vmax=None, title=None):
+    """
+    Plot a 2D matrix with viridis color map
+
+    :param matrix: 2D numpy array
+    :return:
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    if title is not None:
+        ax.set_title(title)
+    plt.imshow(matrix, vmin=vmin, vmax=vmax, aspect="auto")
+    plt.colorbar()
+    if output_image_path:
+        plt.savefig(str(output_image_path), dpi=200)
+    else:
+        plt.show()
+    plt.close(fig)
